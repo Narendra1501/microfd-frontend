3f4ef69cb8b40ad88ba3c67a1ec41b42547c940c
Naredra1501 <narendraramesh15@gmail.com>
2026-04-05 16:15:49 +0530

    Update: baseurl changed

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,9 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://127.0.0.1:5000/api',
+    baseURL: 'https://microfd-backend.onrender.com/api',
+    // baseURL: 'http://127.0.0.1:5000/api',
+
 });
 
 // Request interceptor to attach JWT token
